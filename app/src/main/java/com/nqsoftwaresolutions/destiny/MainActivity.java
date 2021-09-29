96fa8e5e088e0023121282c9d3b008ee13eea04f
Abdul Qayoom Jat <abdulqayoomjat2470@gmail.com>
2021-09-29 08:05:40 +0500

    One step is set...

--- app/src/main/java/com/nqsoftwaresolutions/destiny/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/destiny/MainActivity.java
@@ -1,13 +1,18 @@
 package com.nqsoftwaresolutions.destiny;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    private TextView mStoryTv;
+    private Button mRedBtn;
+    private Button mBlueBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+            mStoryTv = findViewById(R.id.storyTextView);
+            mRedBtn = findViewById(R.id.buttonTop);
+            mBlueBtn = findViewById(R.id.buttonBottom);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
+            mRedBtn.setOnClickListener(v ->{
+                mStoryTv.setText(R.string.T2_Story);
+            });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+            mBlueBtn.setOnClickListener(v ->{
+                mStoryTv.setText(R.string.T3_Story);
+            });
 
     }
 }
