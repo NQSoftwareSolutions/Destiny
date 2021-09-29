998709f4e2173c0c62ee368e5af231bb2368ad6b
Abdul Qayoom Jat <abdulqayoomjat2470@gmail.com>
2021-09-29 12:39:48 +0500

    Revert "Clear."
    This reverts commit 294e5021

--- app/src/main/java/com/nqsoftwaresolutions/destiny/MainActivity.java
+++ app/src/main/java/com/nqsoftwaresolutions/destiny/MainActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,56 +13,48 @@ public class MainActivity extends AppCompatActivity {
     private TextView mStoryTv;
     private Button mRedBtn;
     private Button mBlueBtn;
-    private Stories[] mStoriesBank;
-    private int mRedStory = 1;
-    private int mBlueStory = 1;
+    private int mRedJourney;
+    private int mBlueJourney;
+    private int mStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Todo Retrieve the state of application is if is not null
-            if (savedInstanceState != null){
 
-            }
+
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
             mStoryTv = findViewById(R.id.storyTextView);
             mRedBtn = findViewById(R.id.buttonTop);
             mBlueBtn = findViewById(R.id.buttonBottom);
-        // Todo Init array of stories
-        mStoriesBank = new Stories[]{
-                new Stories(R.string.T1_Story, R.string.T1_Ans1, R.string.T1_Ans2),
-                new Stories(R.string.T2_Story,R.string.T2_Ans1,R.string.T2_Ans2),
-                new Stories(R.string.T3_Story,R.string.T3_Ans1,R.string.T3_Ans2),
-                new Stories(R.string.T4_End),
-                new Stories(R.string.T3_Story,R.string.T3_Ans1,R.string.T3_Ans2),
-                new Stories(R.string.T5_End),
-                new Stories(R.string.T6_End),
-                new Stories(R.string.T5_End),
-                new Stories(R.string.T6_End)
-        };
+
+
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
             mRedBtn.setOnClickListener(v ->{
-                if (mRedStory == 1){
-                    mStoryTv.setText(mStoriesBank[2].getStory());
-                    mRedBtn.setText(mStoriesBank[2].getAnswer1());
-                    mBlueBtn.setText(mStoriesBank[2].getAnswer2());
-                    mRedStory++;
-                }else if (mBlueStory == 2){
-                    mStoryTv.setText(mStoriesBank[4].getStory());
-                    mRedBtn.setText(mStoriesBank[4].getAnswer1());
-                    mBlueBtn.setText(mStoriesBank[4].getAnswer2());
-                    mRedStory++;
-                }else if(mRedStory == 3){
+                if (mRedJourney == 0){
                     mStoryTv.setText(R.string.T3_Story);
                     mRedBtn.setText(R.string.T3_Ans1);
                     mBlueBtn.setText(R.string.T3_Ans2);
-                    mRedStory++;
-                }else if(mRedStory == 4){
+                    mRedJourney++;
+                    mStory++;
+                }else if (mRedJourney == 1 && mStory == 1){
                     mStoryTv.setText(R.string.T6_End);
                     mBlueBtn.setVisibility(View.INVISIBLE);
                     mRedBtn.setVisibility(View.INVISIBLE);
-                    mRedStory++;
+                    mRedJourney++;
+                    mStory++;
+                }else if(mStory == 1){
+                    mStoryTv.setText(R.string.T3_Story);
+                    mRedBtn.setText(R.string.T3_Ans1);
+                    mBlueBtn.setText(R.string.T3_Ans2);
+                    mRedJourney++;
+                    mStory++;
+                }else if(mStory == 2){
+                    mStoryTv.setText(R.string.T6_End);
+                    mBlueBtn.setVisibility(View.INVISIBLE);
+                    mRedBtn.setVisibility(View.INVISIBLE);
+                    mRedJourney++;
+                    mStory++;
                 }
             });
 
@@ -71,36 +62,32 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
             mBlueBtn.setOnClickListener(v ->{
-                if (mBlueStory == 1){
-                    mStoryTv.setText(mStoriesBank[1].getStory());
-                    mRedBtn.setText(mStoriesBank[1].getAnswer1());
-                    mBlueBtn.setText(mStoriesBank[1].getAnswer2());
-                    mBlueStory++;
-                }else if (mBlueStory == 2) {
-                    mStoryTv.setText(mStoriesBank[3].getStoryEnd());
+                if (mBlueJourney == 0){
+                    mStoryTv.setText(R.string.T2_Story);
+                    mRedBtn.setText(R.string.T2_Ans1);
+                    mBlueBtn.setText(R.string.T2_Ans2);
+                    mBlueJourney++;
+                    mStory++;
+                }else if (mBlueJourney == 1 && mStory == 1) {
+                    mStoryTv.setText(R.string.T4_End);
                     mBlueBtn.setVisibility(View.INVISIBLE);
                     mRedBtn.setVisibility(View.INVISIBLE);
-                    mBlueStory++;
-                }else if (mRedStory == 2) {
-                    mStoryTv.setText(mStoriesBank[5].getStoryEnd());
-                    mBlueBtn.setVisibility(View.INVISIBLE);
-                    mRedBtn.setVisibility(View.INVISIBLE);
-                    mBlueStory++;
-                }else if (mBlueStory == 4){
+                    mBlueJourney++;
+                    mStory++;
+                }else if (mStory == 2) {
                     mStoryTv.setText(R.string.T5_End);
                     mBlueBtn.setVisibility(View.INVISIBLE);
                     mRedBtn.setVisibility(View.INVISIBLE);
-                    mBlueStory++;
+                    mBlueJourney++;
+                    mStory++;
+                }else if (mStory == 1){
+                    mStoryTv.setText(R.string.T5_End);
+                    mBlueBtn.setVisibility(View.INVISIBLE);
+                    mRedBtn.setVisibility(View.INVISIBLE);
+                    mBlueJourney++;
+                    mStory++;
                 }
             });
 
-    }
-
-    /**Todo Saving the state of application
-     * @param outState state of application
-     */
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
     }
 }
